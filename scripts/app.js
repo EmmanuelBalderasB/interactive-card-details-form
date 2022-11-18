77697915ed4e3f503c8300fad27ff09870708eb3
EmmanuelBalderasB <ebalderasbeq@gmail.com>
2022-11-18 02:14:41 -0600

    adds functionality to buttons

--- scripts/app.js
+++ scripts/app.js
@@ -4,18 +4,26 @@ var cleave = new Cleave('#card-number', {
 });
 
  const cardNameInput = document.getElementById('card-name');
-/*const cardNumInput = document.getElementById('card-number');
+ const cardNameDisplay = document.getElementById('card-name-display');
+const cardNumInput = document.getElementById('card-number');
 const cardExpMonthInput = document.getElementById('card-expiry-month');
-const cardExpYearInput = document.getElementById('card-expiry-year'); */
+const cardExpYearInput = document.getElementById('card-expiry-year'); 
+const confirm = document.getElementById('submit-details');
+const continueButton = document.getElementById('continue');
 
+const form = document.forms.item(0)
+
+const displayClass = document.getElementsByClassName('displays');
 const errors = document.getElementsByClassName('error-message');
+const completedContainer = document.getElementById('completion-container');
 
 let inputIds = [];
 let errorIds = [];
+let displayIds = [];
 
-const test = document.forms[0].elements;
+const inputs = document.forms[0].elements;
 
-for (const e of test) {
+for (const e of inputs) {
     inputIds.push(e.id);
 }
 
@@ -23,25 +31,34 @@ for (const e of errors) {
     errorIds.push(e.id);
 }
 
+for (const e of displayClass) {
+    displayIds.push(e.id);
+}
+
+console.log(displayIds);
+
 inputIds.forEach(item => {
     try {
         let el = document.getElementById(item);
         let err = document.getElementById(`${item}-error`)
         // console.log(err)
+        el.addEventListener('focusin', () => {
+            el.style.border = 'solid 2px hsl(249, 99%, 64%)'
+        })
+        el.addEventListener('focusout', () => {
+            el.style.border = 'solid 2px hsl(270, 3%, 87%)'
+        })
         el.addEventListener('mouseout', () => {
             if (el.value === '' || el.value === null) {
                 err.style.display = 'flex';
-                let style = err.style.display;
-                el.style.borderColor = 'hsl(0, 100%, 66%)';
+                //let style = err.style.display;
                 el.style.border = 'solid 1px hsl(0, 100%, 66%)';
-                if (el.id === 'card-expiry-year' || el.id === 'card-expiry-month') {
-                    /* el.style.border = 'solid 1px hsl(0, 100%, 66%)'; */
-                    console.log('here');
-                }
             } else if (el.value !== '' || el.value !== null) {
                 err.style.display = 'none';
+                el.style.border = 'solid 2px hsl(270, 3%, 87%)'
             }
         })
+        
     } catch (error) {
         console.log(error)
     }
@@ -57,4 +74,75 @@ cardNameInput.addEventListener('mouseout', () => {
     };
 })
 
+cardExpMonthInput.addEventListener('mouseout', () => {
+    const el = cardExpMonthInput;
+    if(el.value === '' || el.value === null) {
+        el.style.border = 'solid 1px hsl(0, 100%, 66%)';
+        el.setAttribute("placeholder", "Fill in")
+    } else if (el.value !== '' || el.value !== null) {
+        el.style.border = 'solid 2px hsl(270, 3%, 87%)'
+        el.setAttribute("placeholder", "MM")
+    }
+})
+
+cardExpYearInput.addEventListener('mouseout', () => {
+    const el = cardExpYearInput;
+    if(el.value === '' || el.value === null) {
+        el.style.border = 'solid 1px hsl(0, 100%, 66%)';
+        el.setAttribute("placeholder", "Fill in")
+    } else if (el.value !== '' || el.value !== null) {
+        el.style.border = 'solid 2px hsl(270, 3%, 87%)'
+        el.setAttribute("placeholder", "YY")
+    }
+})
+
 console.log(inputIds);
+
+inputIds.forEach(item => {
+    try {
+        const el = document.getElementById(item); 
+        const currentDisplay = document.getElementById(`${item}-display`);
+        el.addEventListener('input', () => {
+            currentDisplay.innerText = el.value
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+confirm.addEventListener('click', (e) => {
+    try {
+        /* let isEmpty = true;
+        inputIds.forEach(id => {
+            let el = document.getElementById('id');
+            if (el.value !== '' || el.value !== null) {
+                isEmpty = false;
+            } else if (el.value === '' || el.value === null) {
+                isEmpty = true;
+            }
+        })
+        if (!isEmpty) { */
+            e.preventDefault();
+            form.style.display = 'none';
+            completedContainer.style.display = 'inline-block';
+        /* } else {
+            alert('Please fill in all card details');
+        }
+        console.log(isEmpty) */
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+continueButton.addEventListener('click', (e) => {
+    try {
+        // e.preventDefault()
+        form.style.display = 'inline-block';
+        completedContainer.style.display = 'none';
+        form.reset()
+        location.reload();
+    } catch (error) {
+        console.log(error);
+    }
+})
